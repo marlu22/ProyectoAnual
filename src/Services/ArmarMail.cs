48881616f0639af839f7c49928d51438a6170a32
Lucas S Ramos <40727001+HelloMeow10@users.noreply.github.com>
2025-07-24 20:02:26 -0300

    Mailer

---
+++ src/Services/ArmarMail.cs
@@ -0,0 +1,20 @@
+﻿namespace CapaSesion
+{
+    public static class ArmarMail
+    {
+        public static string DireccionCorreo { get; set; }
+        public static string Asunto { get; set; }
+        public static string NuevaContraseña { get; set; }
+
+        public static void Preparar()
+        {
+            string body = @"<style>
+                            h1{color:dodgerblue;}
+                            h2{color:darkorange;}
+                            </style>
+                            <h1> Contraseña de ingreso: </h1></br>
+                            <h2> " + NuevaContraseña + "</h2>";
+            EnviarMail.sendMail(DireccionCorreo, Asunto, body);
+        }
+    }
+}
